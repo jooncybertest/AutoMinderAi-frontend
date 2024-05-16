6d201166185d7df1437cf64bb0bbb33f826087ce
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-05-16 15:42:49 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,9 @@ import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={new QueryClient()}>
